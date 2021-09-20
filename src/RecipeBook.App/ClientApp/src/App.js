e55a28cc4e4013de934e1112c9df9e21825a60fa
Bjørn Thorlacius <bjorn.thorlacius@knowit.no>
2021-09-20 10:52:43 +0200

    npm --upgrade && npm install --force
    We upgraded to react 17, however some dependencies were unable to resolve without "--force", we might have issues now.

--- src/RecipeBook.App/ClientApp/src/App.js
+++ src/RecipeBook.App/ClientApp/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { Route } from "react-router";
+import { Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
-import { Home } from "./components/Home";
 import "./custom.css";
+import WelcomePage from "./pages/WelcomePage";
 
 export default class App extends Component {
   static displayName = App.name;
@@ -10,7 +10,7 @@ export default class App extends Component {
   render() {
     return (
       <Layout>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={WelcomePage} />
       </Layout>
     );
   }
